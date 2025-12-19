29482397bc50a4c6ffd1fd5627a71c507ec85d4e
Yunyi Wu <yw1688@g.rit.edu>
2025-12-19 15:21:55 +0700

    Initial Commit

---
+++ Awesome Chess/Awesome_ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Awesome_ChessApp.swift
+//  Awesome Chess
+//
+//  Created by Yunyi Wu on 19.12.2025..
+//
+
+import SwiftUI
+
+@main
+struct Awesome_ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
